ca10011f97ac2010469fea166eb24e2c3705854d
Jordan Heemskerk <jordanh@shaw.ca>
2015-03-26 21:51:45 -0700

    Fixup create table statements

--- src/createTables.sql
+++ src/createTables.sql
@@ -1,124 +1,149 @@
-CREATE TABLE Airline(code CHAR(5) PRIMARY KEY, 
-			name VARCHAR(255), 
-			website VARCHAR(255));
+CREATE TABLE Airline
+  (
+     code         CHAR(5) PRIMARY KEY,
+     airline_name VARCHAR(255),
+     website      VARCHAR(255)
+  );
 
-CREATE TABLE PlaneModel(code CHAR(5) PRIMARY KEY, 
-			INT capacity);
+CREATE TABLE PlaneModel
+  (
+     code     CHAR(5) PRIMARY KEY,
+     capacity INT
+  );
 
-CREATE TABLE Flight(number CHAR(5), 
-			VARCHAR(255) source, 
-			VARCHAR(255) destination);
+CREATE TABLE Flight
+  (
+     num      CHAR(5) PRIMARY KEY,
+     src      VARCHAR(255),
+     destination VARCHAR(255)
+  );
 
-CREATE TABLE Operates(airlineCode CHAR(5) FOREIGN KEY REFERENCES Airline(code),
-			planeModelCode CHAR(5) FOREIGN KEY REFERENCES PlaneModel(code),
-			flightNumber CHAR(5) FOREIGN KEY REFERENCES Flight(number));
-			
-CREATE TABLE IncomingFlight(flightNumber CHAR(5) FOREIGN KEY REFERENCES Flight(number),
-			plannedArrival DATE);
-				
-CREATE TABLE OutgoingFlight(flightNumber CHAR(5) FOREIGN KEY REFERENCES Flight(number),
-			plannedDeparture DATE);
+CREATE TABLE Operates
+  (
+     airlineCode    CHAR(5) REFERENCES Airline(code),
+     planeModelCode CHAR(5) REFERENCES PlaneModel(code),
+     flightNumber   CHAR(5) REFERENCES Flight(num)
+  );
 
-//Insert Arrivals and Departures create table statements
+CREATE TABLE IncomingFlight
+  (
+     num   CHAR(5) PRIMARY KEY,
+     plannedArrival DATE,
+     CONSTRAINT fk_incomingflight
+       FOREIGN KEY (num)
+       REFERENCES Flight(num)
+  );
 
-CREATE TABLE Outgoing(flightNumber CHAR(5) FOREIGN KEY REFERENCES OutgoingFlight(number),
-			departureID INT FOREIGN KEY REFERENCES Departures(id));
-			
-CREATE TABLE Incoming(flightNumber CHAR(5) FOREIGN KEY REFERENCES IncomingFlight(number),
-			arrivalID INT FOREIGN KEY REFERENCES Arrivals(id));
+CREATE TABLE OutgoingFlight
+  (
+     num     CHAR(5) PRIMARY KEY,
+     plannedDeparture DATE,
+     CONSTRAINT fk_outgoingflight
+       FOREIGN KEY (num)
+       REFERENCES Flight(num)
+  );
 
+CREATE TABLE Departures
+  ( 
+    departid INT PRIMARY KEY,
+    gate VARCHAR(8), 
+    departDate DATE 
 
-CREATE TABLE Passenger(
-	passportNumber INT NOT NULL,
-	name VARCHAR(50),
-	dateOfBirth DATE,
-	placeOfBirth VARCHAR(50),
-	citizenship CHAR(3),
-	PRIMARY KEY(passportNumber)
+  );
+
+CREATE TABLE Arrivals ( 
+    gate VARCHAR(8), 
+    arriveDate DATE, 
+    arriveid INT PRIMARY KEY
 );
 
+CREATE TABLE Outgoing
+  (
+     flightNumber CHAR(5) REFERENCES OutgoingFlight(num),
+     departureID  INT REFERENCES Departures(departid)
+  );
+
+CREATE TABLE Incoming
+  (
+     flightNumber CHAR(5) REFERENCES IncomingFlight(num),
+     arrivalID    INT REFERENCES Arrivals(arriveid)
+  );
+
+CREATE TABLE Passenger
+  (
+     passportNumber INT NOT NULL,
+     passengername           VARCHAR(50),
+     dateOfBirth    DATE,
+     placeOfBirth   VARCHAR(50),
+     citizenship    CHAR(3),
+     PRIMARY KEY(passportNumber)
+  );
+
+
 CREATE TABLE FirstClass(
-	passportNumber INT NOT NULL,
+	passportNumber INT REFERENCES Passenger(passportNumber),
 	cost DECIMAL(3,2),
-	meal VARCHAR(20),
-	FOREIGN KEY passportNumber REFERENCES Passenger(passportNumber)
+	meal VARCHAR(20)
 );
 
 CREATE TABLE Economy(
-	passportNumber INT NOT NULL,
-	cost DECIMAL(3,2),
-	FOREIGN KEY passportNumber REFERENCES Passenger(passportNumber)
+	passportNumber INT REFERENCES Passenger(passportNumber),
+	cost DECIMAL(3,2)
 );
 
 CREATE TABLE Infant(
-	passportNumber INT NOT NULL,
+	passportNumber INT REFERENCES Passenger(passportNumber),
 	cost DECIMAL(3,2),
-	age INT,
-	FOREIGN KEY passportNumber REFERENCES Passenger(passportNumber)
+	age INT
 );
 
 CREATE TABLE SpecialNeeds(
-	passportNumber INT NOT NULL,
+	passportNumber INT REFERENCES Passenger(passportNumber),
 	cost DECIMAL(3,2),
-	equipment VARCHAR(20),
-	FOREIGN KEY passportNumber REFERENCES Passenger(passportNumber)
+	equipment VARCHAR(20)
 );
 
 CREATE TABLE Senior(
-	passportNumber INT NOT NULL,
+	passportNumber INT REFERENCES Passenger(passportNumber),
 	cost DECIMAL(3,2),
-	age INT,
-	FOREIGN KEY passportNumber REFERENCES Passenger(passportNumber)
-);
-
-CREATE TABLE Departures (
-    gate VARCHAR(8),
-    departDate DATE,
-    id INT AUTO_INCREMENT
-);
-
-CREATE TABLE Arrivals (
-    gate VARCHAR(8),
-    arriveDate DATE,
-    id INT AUTO_INCREMENT
+	age INT
 );
 
 
 CREATE TABLE DepartureStatus (
     info VARCHAR(255),
-    time DATE,
-    id INT AUTO_INCREMENT
+    id INT PRIMARY KEY
 );
 
 CREATE TABLE ArrivalStatus (
     info VARCHAR(255),
-    time DATE,
-    id INT AUTO_INCREMENT
+    id INT PRIMARY KEY
 );
 
-CREATE TABLE DepartureHasStatus (
-    statusId INT FOREIGN KEY REFERENCES DepartureStatus(id),
-    departureId INT FOREIGN KEY REFERENCES Departures(id)
-);
+CREATE TABLE DepartureHasStatus
+  (
+     statusId    INT REFERENCES DepartureStatus(id),
+     time DATE,
+     departureId INT  REFERENCES Departures(departid)
+  );
 
-CREATE TABLE ArrivalHasStatus (
-    statusId INT FOREIGN KEY REFERENCES DepartureStatus(id),
-    arrivalId INT FOREIGN KEY REFERENCES Arrivals(id)
-);
+CREATE TABLE ArrivalHasStatus
+  (
+     statusId  INT REFERENCES DepartureStatus(id),
+     time DATE,
+     arrivalId INT REFERENCES Arrivals(arriveid)
+  );
 
-CREATE TABLE onDepart (
-    departureId INT FOREIGN KEY REFERENCES Departures(id),
-    passenger INT FOREIGN KEY REFERENCES Passenger(passportnum)
-);
+CREATE TABLE onDepart
+  (
+     departureId INT REFERENCES Departures(departid),
+     passenger   INT REFERENCES Passenger(passportnumber)
+  );
 
-CREATE TABLE onArrival (
-    arrivalId INT FOREIGN KEY REFERENCES Arrivals(id),
-    passenger INT FOREIGN KEY REFERENCES Passenger(passportnum)
-);
+CREATE TABLE onArrival
+  (
+     arrivalId INT REFERENCES Arrivals(arriveid),
+     passenger INT REFERENCES Passenger(passportnumber)
+  );
 
-CREATE TABLE Baggage (
-    flightNum CHAR(5) FOREIGN KEY REFERENCES Flight(number),
-    weight INT,
-    passenger INT FOREIGN KEY REFERENCES Passenger(passportnum),
-    id INT AUTO_INCREMENT
-);
+
